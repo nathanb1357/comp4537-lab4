8394216607893ea31f9cdf72f202bd79a5f1fe1e
sbeeve2023 <cgoss3@my.bcit.ca>
2024-10-08 19:16:16 -0700

    Put client side javascript in a classes

--- client/js/search.js
+++ client/js/search.js
@@ -2,33 +2,37 @@ document.getElementById('button').innerHTML = MESSAGES.search;
 document.getElementById('title').innerHTML = MESSAGES.search;
 
 
-document.getElementById('button').onclick = function() {
-    XMLHttpRequestObject = new XMLHttpRequest();
-    XMLHttpRequestObject.onreadystatechange = function() {
-        if (XMLHttpRequestObject.readyState == 4 && XMLHttpRequestObject.status == 200) {
-            let data = JSON.parse(XMLHttpRequestObject.responseText);
+class Search {
+    constructor() {
+        document.getElementById('button').onclick = function () {
+            XMLHttpRequestObject = new XMLHttpRequest();
+            XMLHttpRequestObject.onreadystatechange = function () {
+                if (XMLHttpRequestObject.readyState == 4 && XMLHttpRequestObject.status == 200) {
+                    let data = JSON.parse(XMLHttpRequestObject.responseText);
 
-            // format search result into text
-            let searchResult = MESSAGES.searchResultSuccess.replace('{count}', data.requestCount);
-            searchResult = searchResult.replace('{word}', document.getElementById('search').value);
-            searchResult = searchResult.replace('{definition}', data.definition);
+                    // format search result into text
+                    let searchResult = MESSAGES.searchResultSuccess.replace('{count}', data.requestCount);
+                    searchResult = searchResult.replace('{word}', document.getElementById('search').value);
+                    searchResult = searchResult.replace('{definition}', data.definition);
 
-            document.getElementById('content').innerHTML = searchResult;
+                    document.getElementById('content').innerHTML = searchResult;
+                }
+
+                if (XMLHttpRequestObject.readyState == 4 && XMLHttpRequestObject.status == 404) {
+                    let data = JSON.parse(XMLHttpRequestObject.responseText);
+
+                    document.getElementById('content').innerHTML = data["message"];
+                }
+            }
+
+            //if word isn't string without numbers give error
+            if (document.getElementById('search').value.match(/[^a-zA-Z]/)) {
+                document.getElementById('content').innerHTML = MESSAGES.regexError;
+            } else {
+                let word = document.getElementById('search').value;
+                XMLHttpRequestObject.open('GET', "https://nbartyuk.site/?word=" + word, true);
+                XMLHttpRequestObject.send();
+            }
         }
-
-        if (XMLHttpRequestObject.readyState == 4 && XMLHttpRequestObject.status == 404) {
-            let data = JSON.parse(XMLHttpRequestObject.responseText);
-            
-            document.getElementById('content').innerHTML = data["message"];
-        }
-    }
-
-    //if word isn't string without numbers give error
-    if (document.getElementById('search').value.match(/[^a-zA-Z]/)) {
-        document.getElementById('content').innerHTML = MESSAGES.regexError;
-    } else {
-        let word = document.getElementById('search').value;
-        XMLHttpRequestObject.open('GET', "https://nbartyuk.site/?word=" + word, true);
-        XMLHttpRequestObject.send();
     }
 }
--- client/js/store.js
+++ client/js/store.js
@@ -2,24 +2,31 @@ document.getElementById('button').innerHTML = MESSAGES.submit;
 document.getElementById('title').innerHTML = MESSAGES.store;
 
 
-document.getElementById('button').onclick = function() {
-    let word = document.getElementById('word').value;
-    let definition = document.getElementById('definition').value;
 
-    XMLHttpRequestObject = new XMLHttpRequest();
-    XMLHttpRequestObject.onreadystatechange = function() {
-        if (XMLHttpRequestObject.status == 404 || XMLHttpRequestObject.status == 400) {
-            data = JSON.parse(XMLHttpRequestObject.responseText);
-            document.getElementById('content').innerHTML = data["message"];
-        }
-        if (XMLHttpRequestObject.readyState == 4 && XMLHttpRequestObject.status == 201) {
-            data = JSON.parse(XMLHttpRequestObject.responseText);
-            document.getElementById('content').innerHTML = data["message"];
+class Store {
+    constructor() {
+        document.getElementById('button').onclick = function () {
+            let word = document.getElementById('word').value;
+            let definition = document.getElementById('definition').value;
+
+            XMLHttpRequestObject = new XMLHttpRequest();
+            XMLHttpRequestObject.onreadystatechange = function () {
+                if (XMLHttpRequestObject.status == 404 || XMLHttpRequestObject.status == 400) {
+                    data = JSON.parse(XMLHttpRequestObject.responseText);
+                    document.getElementById('content').innerHTML = data["message"];
+                }
+                if (XMLHttpRequestObject.readyState == 4 && XMLHttpRequestObject.status == 201) {
+                    data = JSON.parse(XMLHttpRequestObject.responseText);
+                    document.getElementById('content').innerHTML = data["message"];
+                }
+            }
+
+            let data = { word: word, definition: definition };
+
+            XMLHttpRequestObject.open('POST', "https://nbartyuk.site", true);
+            XMLHttpRequestObject.send(JSON.stringify(data));
         }
     }
-
-    let data = {word: word, definition: definition};
-
-    XMLHttpRequestObject.open('POST', "https://nbartyuk.site", true);
-    XMLHttpRequestObject.send(JSON.stringify(data));
 }
+
+new Store();
